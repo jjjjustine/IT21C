bd6251124ebe88811a36153d06452350c7320de7
jjjjustine <catingan.janejustine@gmail.com>
2024-10-04 20:36:01 +0800

    Added a Person's age and occupation then a console.log to print the Person's age and occupation

--- Topic-2-Methods.js
+++ Topic-2-Methods.js
@@ -1,8 +1,12 @@
 //Input
 const Person ={
     name:"Jane Justine",
-
+    age:19,
+    occupation:"Student"
 }
 
 //Process and Output
 console.log("Name: "+Person.name);
+console.log("Age: "+Person.age);
+console.log("Occupation: "+Person.occupation);
+
