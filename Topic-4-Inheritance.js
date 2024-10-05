3d55118b9d5119033f14c6e516d715d42ad4af6a
jjjjustine <catingan.janejustine@gmail.com>
2024-10-05 16:57:55 +0800

    Added a class Person with a private data fields which is name, age, occupation, and year

---
+++ Topic-4-Inheritance.js
@@ -0,0 +1,15 @@
+class Person{
+
+    //Private Fields
+    #name;
+    #age;
+    #occupation;
+    #year;
+
+    constructor(name, age, occupation, year){
+        this.#name = name;
+        this.#age = age;
+        this.#occupation = occupation;
+        this.#year = year;
+    }
+}
